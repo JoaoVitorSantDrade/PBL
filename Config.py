7743986e3eb920dd02cd08ce12cffa64817b254a
JoaoVitorSantDrade <jvitorsantdrade@gmail.com>
2022-09-11 14:40:57 -0300

    Att 11/09

--- Config.py
+++ Config.py
@@ -1,9 +1,12 @@
 PORT = 3249 # Porta utilizada. Acima de 1024 e escolhida aleatoriamente
-HOST = "0.0.0.0" # IP que iremos conectar o servidor e o cliente
+PORT_EXTERNA = 120 # Porta utilizada para as requisições
+HOST = "localhost" # IP que iremos conectar o servidor e o cliente
+HOST_EXTERNA = "localhost" # Endereço de IP para requisições externas
 PAYLOAD_SIZE = 1480 # Multiplos de 8
 TESTE = True # Indica se estamos debugando
 SERVIDOR = False # Indica se iremos rodar um Servidor ou um Cliente
 SERVER_LISTEN = 32 # Numero de conexões maxima pelo servidor
 TIMEOUT = 60 # Tempo de timeout em segundos
+TIMEOUT_EXTERNO = 120
 PACKET_SIZE = 512 # Tamanho de cada pacote
 TEST_MESSAGE = "Testando o envio de strings"
--- Socket.py
+++ Socket.py
@@ -2,9 +2,8 @@ from asyncio.windows_events import NULL
 import socket
 import Config
 import json
-import requests
-from http.server import BaseHTTPRequestHandler, HTTPServer
-
+from datetime import datetime
+import codecs
 from Hidrante import Hidrante
 
 class Client:
@@ -55,38 +54,14 @@ class Client:
             print("Fechando conexão com o servidor")
             sock.close
 
-class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-Type', 'application/json')
-        self.end_headers()
-        message = "Isso é uma chamada usando GET"
-        self.wfile.write(bytes(message, "utf8"))
-
-    def do_POST(self):
-        self.send_response(200)
-        self.send_header('Content-Type','application/json')
-        self.end_headers
-
-
 class Server:
-    host = Config.HOST
-    port = Config.PORT
     payload_size = Config.PAYLOAD_SIZE
     hid_list = {}
 
-    def __init__(self,host,port):
-        self.host = host
-        self.port = port
-
-    def alterar_endereco(self,host,port):
-        self.host = host
-        self.port = port
-
-    def rodar_servidor(self):
+    def serverTCP(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
-        servidor = (self.host, self.port)
+        servidor = (Config.HOST, Config.PORT)
         sock.bind(servidor)
 
         print("Servidor iniciando no endereço %s:%s" % servidor)
@@ -99,30 +74,57 @@ class Server:
                 data = client.recv(Config.PAYLOAD_SIZE) #Recebemos bytes de um Json encoded em utf-8
                 if data:
                     client.send(data)  #Reenviamos os bytes do Json
-                    print("Mensagem recebida!")
                     data = data.decode() #Decodificamos os bytes utf-8
+                    print("Mensagem recebida!: %s" % data)
                     data = json.loads(data) #Carregamos os bytes dentro de um Json
                     Server.add_to_list(self,data) #Passamos o Json para ser adicionado a lista de Hidrantes do servidor
                     client.close()
-                    # end connection
-                    with open('hidrometro.json','w') as file:
-                        json.dump(self.hid_list,file)
-                        #https://flaviocopes.com/python-http-server/
-                        #https://stackoverflow.com/questions/28240464/python-http-server-send-json-response
-                        #https://gist.github.com/nitaku/10d0662536f37a087e1b
-                        #https://stackoverflow.com/questions/45151473/returning-json-using-a-get-request-from-server
-                        #Usar o dump pra salvar todos os hidrantes num arquivo Json unico. esse arquivo json unico é oq vai pra web
-            except Exception as err: 
-                print("O tempo de espera do servidor foi excedido! - %s" % err)
+            
+            except TimeoutError as errt: 
+                print("O tempo de espera do servidor foi excedido! - %s" % errt)
                 print(self.hid_list)
-                with HTTPServer(('', 8000), handler) as server:
-                    server.serve_forever()
                 break
 
+    def http_serverTCP(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+        servidor = (Config.HOST_EXTERNA, Config.PORT_EXTERNA)
+        sock.bind(servidor)
+
+        print("Servidor externo iniciando no endereço %s:%s" % servidor)
+        sock.listen(Config.SERVER_LISTEN)
+        sock.settimeout(Config.TIMEOUT_EXTERNO)
+        while True:
+            try:
+                print ("Aguardando mensagens de clientes...")
+                client, address = sock.accept() #Espera receber algum pacote
+                data = client.recv(Config.PAYLOAD_SIZE) #Recebemos bytes de uma requisição http
+                if data:
+                    request = Server.normalize_line_endings(data.decode()) 
+                    request_head, request_body = request.split('\n\n', 1)
+                    print("Head do request:\n%s " % request_head)
+                    print("Body do request:\n%s" % request_body)
+                    horario = datetime.now().strftime("%H:%M:%S")
+                    response = Server.http_header(horario,Config.HOST_EXTERNA,"text/html; charset=utf-8")
+                    html_template = codecs.open("template.html", 'r','utf-8').read()
+                    response = response + html_template
+                    client.sendall(response.encode("utf-8"))
+                    client.close()
+            except TimeoutError as errt: 
+                print("O tempo de espera do servidor foi excedido! - %s" % errt)
+                break
+
+    def http_header(Date,Servidor,Content_type):
+        return "HTTP/1.1 200 OK \r\nDate: "+ Date +"\r\nServer: "+ Servidor +"\r\nContent-Type: "+Content_type+"\r\n\r\n"
+        
+    def normalize_line_endings(s):
+        return ''.join((line + '\n') for line in s.splitlines())
+
     def add_to_list(self,Json):
-        Server.hid_list[Json["ID"]] = json.dumps(Json) # Salva o Json em formato string numa Lista
+        if Server.hid_list[Json["ID"]] == NULL:
+            Server.hid_list[Json["ID"]] = json.dumps(Json) # Salva o Json em formato string numa Lista
         
 
 if __name__ == '__main__':
-    servidor = Server(Config.HOST, Config.PORT)
-    servidor.rodar_servidor()
+    servidor = Server()
+    servidor.http_serverTCP()
